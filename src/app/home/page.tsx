08a5f9036fe45025cf28f2962e2780e6384dded8
Elena-Khrychenkova <khry0001@algonquinlive.com>
2025-07-07 15:24:42 -0400

    Home page updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/app/auth";
 import { Button } from "@/components/ui/button";
 import SignOut from "@/components/ui/signout";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 export default async function HomePage() {
   const session = await auth();
   if (!session) redirect("/login");
@@ -11,23 +12,58 @@ export default async function HomePage() {
     //   <SignOut />
     // </div>
     <div>
-      <div className="flex items-center justify-center h-[520px] px-[170px] py-[60px] gap-[60px] bg-amber-500">
-        <div className="flex flex-col w-[520px] h-[96px] gap-[24px] bg-green-300 ">
+      <div className="flex items-center justify-center h-[520px] px-[170px] py-[60px] gap-[60px]">
+        <div className="flex flex-col w-[520px] h-[96px] gap-[24px]">
           <h1 className="font-bold text-[40px] h-[48px]">
             Welcome, {session.user?.name}!
           </h1>
           <p>Explore how your lifestyle choices impact your food intake.</p>
         </div>
         <div>
-          <div className="bg-blue-300 w-[520px] h-[400px]">Hello</div>
+          <div className="flex flex-col">
+            <Image
+              src="/pixeltrue-healthy-eating 1.png"
+              width={520}
+              height={400}
+              alt="Imafge of a woman"
+            />
+            <p className="flex justify-end text-sm">
+              Illustration by&nbsp;
+              <a href="https://icons8.com/illustrations/author/ARh4OKrFtdfC">
+                Pixeltrue Ouch!
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col items-center h-[944px] border-t-2 border-b-2 border-gray-400  px-[170px] py-[60px] gap-[60px] bg-violet-300">
-        <div className="flex flex-col items-center h-[168px] w-[1100px] gap-[24px] bg-amber-800 ">
+      <div className="flex flex-col items-center border-t-2 border-b-2 border-gray-200  px-[170px] py-[60px] gap-[60px]">
+        <div className="flex flex-col items-center h-[168px] w-[1100px] gap-[24px]">
           <h1 className="font-bold text-[40px] h-[48px]">Your Data Metrics</h1>
           <p>Explore how harious factors influence your food choices</p>
           <Button className="w-[240px] h-[48px]">View All Metrics</Button>
         </div>
+        <div>
+          <Image
+            src="/food_intake_diagram.svg"
+            width={1100}
+            height={440}
+            alt="Food Intake vs. Activities Diagram"
+          />
+        </div>
+        <ol className="flex w-[1100px] h-[96px] justify-between">
+          <li className="flex flex-col w-[353.33px] h-[96px] border border-gray-400 py-[16px] px-[16px] gap-[6px] rounded-xl">
+            <p>Sleep</p>
+            <p>6 hours</p>
+          </li>
+          <li className="flex flex-col w-[353.33px] h-[96px] border border-gray-400 py-[16px] px-[16px] gap-[6px] rounded-xl">
+            <p>Sport</p>
+            <p>2 hours</p>
+          </li>
+          <li className="flex flex-col w-[353.33px] h-[96px] border border-gray-400 py-[16px] px-[16px] gap-[6px] rounded-xl">
+            <p>Hydration</p>
+            <p>8 hours</p>
+          </li>
+        </ol>
       </div>
     </div>
   );
